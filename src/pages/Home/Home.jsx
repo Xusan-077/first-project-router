64c96d8ec1bb6a44320e3205e2337610fdcfc6e0
XusanYarashov <xusanyarashov1@gmail.com>
2025-10-31 22:46:13 +0500

    update project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import HomeHero from "../../components/HomeHero/HomeHero";
 import Locol from "../../components/Locol/Locol";
 import Subscribe from "../../components/Subscribe/Subscribe";
-import Information from "../../components/information/information";
+import Information from "../../components/Information/Information";
 
 export default function Home() {
   return (
